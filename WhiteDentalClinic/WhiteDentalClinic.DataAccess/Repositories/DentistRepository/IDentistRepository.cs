242ee9a4402ef7a58c8fdc3b94b4f662293eb09a
pikp <sorinnnn96@yahoo.com>
2023-03-27 09:56:16 +0200

    Update entities

--- WhiteDentalClinic/WhiteDentalClinic.DataAccess/Repositories/DentistRepository/IDentistRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using WhiteDentalClinic.DataAccess.Entities.DentistEntity;
-using WhiteDentalClinic.DataAccess.Repositories.BaseRepositories;
-
-namespace WhiteDentalClinic.DataAccess.Repositories.DentistRepository
-{
-    public interface IDentistRepository : IBaseRepository<Dentist>
-    {
-    }
-}
