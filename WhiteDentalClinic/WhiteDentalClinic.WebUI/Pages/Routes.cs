7b6314bd471d861fdf312cea0db84f572f1d5e30
pikp <sorinnnn96@yahoo.com>
2023-03-29 19:38:37 +0200

    Front-end and services

--- WhiteDentalClinic/WhiteDentalClinic.WebUI/Pages/Routes.cs
+++ WhiteDentalClinic/WhiteDentalClinic.WebUI/Pages/Routes.cs
@@ -5,6 +5,7 @@
         public const string About = "about";
         public const string ProfileAppointments = "appointments";
         public const string Dentists = "dentists";
+        public const string DentistInfo = "info";
         public const string Error = "error";
         public const string Index = "index";
         public const string NewAppointment = "newappointment";
